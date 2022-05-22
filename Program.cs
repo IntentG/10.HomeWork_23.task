7c490bfda35eb54df653035ff1a174ba13c59aa9
Dmitriy Gerassimov <103821065+IntentG@users.noreply.github.com>
2022-05-22 20:54:37 +0600

    Update Program.cs
    Добавил условный оператор для проверки натуральности числа.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
-﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
 
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
-int cube = 0;
+int cube = 1;
 
-int CubeNumber (int number)
+int CubeNumber(int number)
 {
-    int cube = 1;
-    for (int i = 1; i <= number; i++)
+        if (number > 0)
         {
-            cube = (i * i * i);
-            Console.Write($"| {cube, 2}");
+            for (int i = 1; i <= number; i++)
+                {
+                    cube = i * i * i;
+                    Console.Write($"| {cube,2}");
+                }
+            Console.Write($"   -> Таблица кубов чисел от 1 до {number}");
+        }
+        else
+        {
+            Console.WriteLine("Расчёт будет произведён от 1.\nВведите число больше 0");
         }
     return cube;
 }
 
-int result = CubeNumber (number);
-Console.Write($"   -> Произведение кубов чисел от 1 до {number}");
+int result = CubeNumber(number);
